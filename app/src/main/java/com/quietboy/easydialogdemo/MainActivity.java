5f0fc75bcd7d4f50d14dad8f303a89dca5802de0
quietsboy <quietboy66@163.com>
2017-06-02 14:23:16 +0800

    初始上传

--- app/src/main/java/com/quietboy/easydialogdemo/MainActivity.java
+++ app/src/main/java/com/quietboy/easydialogdemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.quietboy.easydialogdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.quietboy.easydialog.widget.AlertDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new AlertDialog.Builder(this)
+                .setTitle("sdfsdf")
+                .setMessage("sdgsdg")
+                .setPositiveButton("dsgsg", R.color.colorAccent, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }).show();
     }
 }
